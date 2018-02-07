57f95769daeccba4c881fc1200471f5ea1bd26c0
Abraham <abrahamcv90@gmail.com>
2018-02-07 13:03:23 +0000

    Segundo Programa

--- U6/a2/cal1.rb
+++ U6/a2/cal1.rb
@@ -5,20 +5,20 @@ op = ARGV[1]
 num2 = ARGV[2].to_i
 
 if ARGV.size < 3 
-	puts "Tienes que poner dos argumentos"
-	exit
+  puts "Tienes que poner dos argumentos"
+  exit
 end
 
 if op == "+"
-	Result_plus = num1 + num2
-	puts "El resultado de #{num1} y #{num2} es #{Result_plus}"
+  Result_plus = num1 + num2
+  puts "El resultado de #{num1} y #{num2} es #{Result_plus}"
 elsif op == "-"
-	Result_minus = num1 - num2
-	puts "El resultado de #{num1} y #{num2} es #{Result_minus}"
+  Result_minus = num1 - num2
+  puts "El resultado de #{num1} y #{num2} es #{Result_minus}"
 elsif op == "x"
-	Result_multi = num1 * num2
-	puts "El resultado de #{num1} y #{num2} es #{Result_multi}"
+  Result_multi = num1 * num2
+  puts "El resultado de #{num1} y #{num2} es #{Result_multi}"
 elsif op == "/"
-	Result_div = num1 / num2
-	puts "El resultado de #{num1} y #{num2} es #{Result_div}"
+  Result_div = num1 / num2
+  puts "El resultado de #{num1} y #{num2} es #{Result_div}"
 end
